e3df07a9bea659a26443703bf1a4850a84d7221f
maney <dp120690miv@gmail.com>
2019-11-10 12:44:06 -0600

    Just starting

--- src/gitHub.java
+++ src/gitHub.java
@@ -1,5 +1,7 @@
 public class gitHub {
     public static void main(String[] args) {
         System.out.println("Some case for test");
+        System.out.println("Coloterorita");
+        System.out.println("sdgdgdfg");
     }
 }
